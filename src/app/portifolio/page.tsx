fe9b5959ae112385b55ac0400b3298cf200369e7
Guilhermedcdias <Guilhermedcdias.2021@gmail.com>
2023-11-23 12:05:21 -0300

    layout terminado

--- src/app/portifolio/page.tsx
+++ src/app/portifolio/page.tsx
@@ -1,7 +1,20 @@
+import Image from "next/image";
+import BaseLayout from "../components/BaseLayout";
+import SwiperCoverflow from "../components/swiper";
+import styles from './page.module.scss'
+
 export default function Portifolio() {
+    const slides = [
+        <Image src="/images/foto2.png" alt="Imagem 2" width={500} height={300} />,
+        <Image src="/images/foto1.png" alt="Imagem 1" width={500} height={300} />,
+        <Image src="/images/foto3.png" alt="Imagem 3" width={500} height={300} />,
+    ]
     return (
-        <div>
-            <h1>Portifolio</h1>
-        </div>
+        <BaseLayout>
+            <div className={styles.main}>
+                <h3>Meus Projetos</h3>
+                <SwiperCoverflow slides={slides} />
+            </div>
+        </BaseLayout>
     )
 }
