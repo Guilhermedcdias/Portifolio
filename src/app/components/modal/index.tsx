a8127d3f141fbf0983385950a407c74e426e378d
Guilhermedcdias <Guilhermedcdias.2021@gmail.com>
2023-11-24 12:51:47 -0300

    text dark modal

--- src/app/components/modal/index.tsx
+++ src/app/components/modal/index.tsx
@@ -1,5 +1,6 @@
 import Button from 'react-bootstrap/Button';
 import Modal from 'react-bootstrap/Modal';
+import styles from './page.module.scss';
 interface IModalProps {
     title: string;
     Projeto: string;
@@ -14,12 +15,12 @@ export default function ModalComponent(props: IModalProps) {
     return (
         <Modal show={props.show} size='xl'>
             <Modal.Header>
-                <Modal.Title>{props.title}</Modal.Title>
+                <Modal.Title className={styles.dark}>{props.title}</Modal.Title>
             </Modal.Header>
             <Modal.Body>
-                <h2>{props.Descricao}</h2>
-                <h5>Tecnologias: {props.Projeto}</h5>
-                <p>{props.Participação}</p>
+                <h2 className={styles.dark}>{props.Descricao}</h2>
+                <h5 className={styles.dark}>Tecnologias: {props.Projeto}</h5>
+                <p className={styles.dark}>{props.Participação}</p>
             </Modal.Body>
             <Modal.Footer>
                 <Button variant="secondary" onClick={props.handleClose}>
